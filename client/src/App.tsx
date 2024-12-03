47bd91e570faec1bb0a94cf8da1f1f88baeee6b0
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-03 18:24:26 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,45 @@
+import { Stack, Typography } from "@mui/material";
+import TextEditor from "./components/TextEditor/TextEditor";
 import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  return <>App</>;
+  return (
+    <>
+      <Navbar />
+      <Stack
+        sx={{
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100%",
+          width: "100%",
+        }}
+      >
+        <Stack sx={{ width: { xs: "95%", md: "80%" } }}>
+          <Typography
+            variant="h2"
+            sx={{ mb: 2 }}
+            component={"h1"}
+            align="center"
+          >
+            CollabBros
+          </Typography>
+          <Typography
+            variant="subtitle1"
+            sx={{ mb: 2 }}
+            component={"h2"}
+            align="center"
+          >
+            Empowering Collaboration, Live Coding, Text Editing and Language
+            sharing web app.
+          </Typography>
+          <TextEditor />
+        </Stack>
+      </Stack>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
