5d6657cf4db97bc1a34b6acf59259d380ccf65ae
https://github.com/PriyanshuGupta28 <28priyanshu2001@gmail.com>
2024-12-18 12:29:38 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,8 @@
 import { Stack, Typography } from "@mui/material";
 import TextEditor from "./components/TextEditor/TextEditor";
-import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
+import "./App.css";
 
 function App() {
   return (
