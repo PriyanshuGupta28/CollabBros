b7b344687ebb749c9e96f618f5d66806460d2037
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-12-19 11:11:26 +0530

    Update roomRoute.js

--- server/routes/roomRoute.js
+++ server/routes/roomRoute.js
@@ -5,12 +5,12 @@ import { joinRoom, codeChange } from "../controllers/roomController.js"; // Impo
 const roomRoute = Router(); // Initialize router
 
 // Example route for checking room status
-roomRoute.get("/", (req, res) => {
-  res.send("CollabEdit Room Routes");
-});
+// roomRoute.get("/", (req, res) => {
+//   res.send("CollabEdit Room Routes");
+// });
 
 // Route to get room details (could be expanded to get room info)
-roomRoute.get("/:roomName", async (req, res) => {
+roomRoute.get("/", async (req, res) => {
   try {
     const room = await Room.findOne({ roomName: req.params.roomName });
     if (!room) {
