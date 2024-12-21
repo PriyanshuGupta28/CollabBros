902a8af5f8c0fa524ef7c84da9bbd17c8c5785e0
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-12-21 11:10:39 +0530

    Update roomModel.js

--- server/models/roomModel.js
+++ server/models/roomModel.js
@@ -1,12 +1,12 @@
-import mongoose from "mongoose"; // Use import for mongoose
+const mongoose = require("mongoose");
 
 const roomSchema = new mongoose.Schema({
-  roomName: { type: String, required: true, unique: true },
-  users: { type: [String], required: true },
-  code: { type: String, default: "" }, // Store the code as a string
+  roomId: { type: String, required: true, unique: true },
+  users: [String],
+  data: String,
+  createdAt: { type: Date, default: Date.now },
 });
 
 const Room = mongoose.model("Room", roomSchema);
 
-// Export the Room model
-export default Room; // Use export default for the model
+module.exports = Room;
