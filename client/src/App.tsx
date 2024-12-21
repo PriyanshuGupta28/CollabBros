fc1b545432ea65f68d05df67e8bc8c4d5b617b77
adarsh <111566026+adarshpatel111@users.noreply.github.com>
2024-12-21 11:10:09 +0530

    Update App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,44 +1,18 @@
-import { Stack, Typography } from "@mui/material";
-import TextEditor from "./components/TextEditor/TextEditor";
 import Navbar from "./components/Navbar/Navbar";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
+import AllRoutes from "./components/Routes/AllRoutes";
+import { Stack } from "@mui/material";
 
 function App() {
   return (
-    <>
+    <Stack sx={{ width: "100%" }}>
       <Navbar />
-      <Stack
-        sx={{
-          justifyContent: "center",
-          alignItems: "center",
-          height: "100%",
-          width: "100%",
-        }}
-      >
-        <Stack sx={{ width: { xs: "95%", md: "80%" } }}>
-          <Typography
-            variant="h2"
-            sx={{ mb: 2 }}
-            component={"h1"}
-            align="center"
-          >
-            CollabBros
-          </Typography>
-          <Typography
-            variant="subtitle1"
-            sx={{ mb: 2 }}
-            component={"h2"}
-            align="center"
-          >
-            Empowering Collaboration, Live Coding, Text Editing and Language
-            sharing web app.
-          </Typography>
-          <TextEditor />
-        </Stack>
+      <Stack sx={{ width: { xs: "100%", md: "80%" }, margin: "0 auto" }}>
+        <AllRoutes />
       </Stack>
       <Footer />
-    </>
+    </Stack>
   );
 }
 
